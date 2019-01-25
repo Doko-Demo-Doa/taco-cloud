d73c78a854ebc1decd22dcd5909dfb387843fc86
Quan Pham <doraemonfanclub@gmail.com>
2019-01-25 18:12:48 +0700

    Initialize tacocloud-api

--- src/main/java/tacos/data/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package tacos.data;
-
-import org.springframework.data.repository.CrudRepository;
-import tacos.Order;
-import tacos.User;
-
-public interface OrderRepository extends CrudRepository<Order, Long> {
-    User findByUsername(String username);
-}
