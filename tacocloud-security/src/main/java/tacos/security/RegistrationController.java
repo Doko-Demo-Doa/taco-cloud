4acd0fdd5e7ac58a77dfebab64eaf1c459e24c83
Quan Pham <doraemonfanclub@gmail.com>
2019-02-03 04:00:44 +0700

    Added code o tacocloud-security module

---
+++ tacocloud-security/src/main/java/tacos/security/RegistrationController.java
@@ -0,0 +1,30 @@
+package tacos.security;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.data.UserRepository;
+
+@Controller
+@RequestMapping
+public class RegistrationController {
+
+    private UserRepository userRepo;
+    private PasswordEncoder passwordEncoder;
+
+    public RegistrationController(UserRepository userRepo, PasswordEncoder passwordEncoder) {
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping
+    public String registerForm() {
+        return "registration";
+    }
+
+    public String processRegistration(RegistrationForm form) {
+        userRepo.save(form.toUser(passwordEncoder));
+        return "redirect:/login";
+    }
+}
